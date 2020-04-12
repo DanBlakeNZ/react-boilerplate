1885fdee6abed7c4783adb0b5d99eee24b14ed8e
DanBlakeNZ <dblakenz@hotmail.com>
2020-04-12 17:51:39 +1000

    webpack recoginises .jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@ const path = require("path");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-module.exports = env => {
+module.exports = (env) => {
   return {
     stats: "errors-only",
     entry: "./src/index.js",
     output: {
       path: path.resolve(__dirname, "public"),
-      publicPath: '/',
-      filename: "bundle.js"
+      publicPath: "/",
+      filename: "bundle.js",
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
     },
     module: {
       rules: [
@@ -17,12 +20,12 @@ module.exports = env => {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
-          }
+            loader: "babel-loader",
+          },
         },
         {
-          test:/\.s?css$/,
-          use:['style-loader','css-loader', 'sass-loader']
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
@@ -31,18 +34,18 @@ module.exports = env => {
               loader: "file-loader",
               options: {
                 name: "[name].[ext]",
-                outputPath: "fonts/"
-              }
-            }
-          ]
+                outputPath: "fonts/",
+              },
+            },
+          ],
         },
-      ]
+      ],
     },
     devServer: {
       contentBase: path.join(__dirname, "public"),
       publicPath: "/dist/",
       historyApiFallback: true,
-      writeToDisk: true
-    }
+      writeToDisk: true,
+    },
   };
 };
